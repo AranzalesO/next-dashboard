c93e1d4fe232562edae127b22bff172bba296e0b
AranzalesO <juanjoaranzales@gmail.com>
2024-02-29 16:47:27 -0500

    feature(dashboard): Populated database, add cards summary, latest 5 invoices and chart for revenue

--- app/page.tsx
+++ app/page.tsx
@@ -16,17 +16,17 @@ export default function Page() {
             className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
           />
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+            <strong>Welcome to Drop.</strong> This is the dashboard app that you need in case you are a{' '}
+            <a href="https://es.wikipedia.org/wiki/Préstamo_gota_a_gota" className="text-blue-500">
+              Gota a gota
             </a>
-            , brought to you by Vercel.
+            , brought to you by Aranzales.
           </p>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Sign in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
